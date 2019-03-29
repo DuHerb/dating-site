e8a491de3e1c89b3147dd89d3edf921a847d3ef6
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-03-28 20:50:25 -0700

    move logic branches into function createID() and clean up code

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,46 @@
 $(document).ready(function(){
 
-    $("form").submit(function(event) {
-      event.preventDefault();
-      var gender = $('input:radio[name=gender]:checked').val();
-      var age = $('input:radio[name=age]:checked').val();
-      var music = $('input:radio[name=music]:checked').val();
-      console.log(gender+age+music);
-      var imgTag = "#";
-      var test = "#rf2";
+  //createID builds a string that can be used to select the
+  function createID(gender, age, music) {
+    var imgID = "#";
 
-      if (music === "rock") {
-        imgTag = imgTag + "r";
-      } else if (music === "techno") {
-        imgTag = imgTag + "t";
-      } else {
-        imgTag = imgTag + "o";
-      }
+    if (music === "rock") {
+      imgID = imgID + "r";
+    } else if (music === "techno") {
+      imgID = imgID + "t";
+    } else {
+      imgID = imgID + "o";
+    }
 
-      if (gender === "male") {
-        imgTag = imgTag + "m";
-      } else {
-        imgTag = imgTag + "f";
-      }
+    if (gender === "male") {
+      imgID = imgID + "m";
+    } else {
+      imgID = imgID + "f";
+    }
 
-      if (age === "18") {
-        imgTag = imgTag + "1";
-      } else if (age === "25") {
-        imgTag = imgTag + "2";
-      } else {
-        imgTag = imgTag + "3";
-      }
+    if (age === "18") {
+      imgID = imgID + "1";
+    } else if (age === "25") {
+      imgID = imgID + "2";
+    } else {
+      imgID = imgID + "3";
+    }
 
-      console.log(imgTag);
+    return imgID
+  };
 
-      $(imgTag).removeClass('hidden');
-    });
+  $("form").submit(function(event) {
+    event.preventDefault();
+
+    var gender = $('input:radio[name=gender]:checked').val();
+    var age = $('input:radio[name=age]:checked').val();
+    var music = $('input:radio[name=music]:checked').val();
+
+    var imgID  = createID(gender, age, music);
+
+    console.log(imgID);
+
+    $(imgID).removeClass('hidden');
+  });
 
 });
